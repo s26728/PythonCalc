dd69db5dd0ad0e57b87771ae79e7c995222e789d
s28314 <s28314@pjwstk.edu.pl>
2024-06-15 16:25:41 +0200

    Fixed addition function, split input field intro 3( current value, operator, next value), added functions for locking buttons as well as styling for disabled buttons and function to execute operation

--- source/Calculator/main.py
+++ source/Calculator/main.py
@@ -5,17 +5,20 @@ app = Flask(__name__)
 
 calculator = Calculator()
 
-@app.route("/Calculator")
+
+@app.route("/calculator")
 def calc():
     global calculator
     return render_template("calculator.html", value=calculator.value)
 
+
 @app.route("/addition", methods=['GET'])
 def addition():
     global calculator
-    x = request.args.get('x')
-    y = request.args.get('y')
+    x = int(request.args.get('x'))
+    y = int(request.args.get('y'))
     return jsonify({'result': calculator.add(x, y)})
+
 
 if __name__ == "__main__":
     app.run()
