247ab06dc097d0c9b09e49805ae49443c3634535
s28314 <s28314@pjwstk.edu.pl>
2024-06-15 14:38:56 +0200

    Added number input

---
+++ source/Calculator/calculator.py
@@ -0,0 +1,18 @@
+class Calculator:
+
+    def __init__(self):
+        self.value = 0
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        if y != 0:
+            return x / y
+        else:
+            return "Cannot divide by zero"
--- source/Calculator/main.py
+++ source/Calculator/main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request
+from calculator import Calculator
 
 app = Flask(__name__)
 
+calculator = Calculator()
 
 @app.route("/calculator")
 def calc():
-    return render_template('calculator.html')
+    global calculator
+    return render_template("calculator.html", value=calculator.value)
 
 
 if __name__ == "__main__":
